56621a6c8054497299f8584494a1bf7858ec51f1
Anusha-17 <98573933+Anusha-17@users.noreply.github.com>
2022-02-16 19:44:34 +0530

    String Copy

---
+++ strcpy.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+     char s1[30] = "String 1";
+     char s2[30] = "Anusha";
+     strcpy(s1,s2);
+     printf("String s1 is: %s", s1);
+     return 0;
+}
